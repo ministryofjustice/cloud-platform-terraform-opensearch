a38dbfcf878170826cc66f664c45cf893006a2cb
Jake Mulley <jakemulley@users.noreply.github.com>
2023-08-03 10:10:12 +0100

    Be explicit the SA is for OpenSearch

--- irsa.tf
+++ irsa.tf
@@ -31,7 +31,7 @@ module "irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = local.identifier
+  service_account_name = "${local.identifier}-opensearch"
   namespace            = var.namespace # this is also used as a tag
   role_policy_arns = {
     opensearch = aws_iam_policy.irsa.arn
