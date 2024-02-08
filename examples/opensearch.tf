c78e8a3b1217a8dd5a6616eaf915a7e4fe852d33
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-02-08 14:59:32 +0000

    update examples

--- examples/opensearch.tf
+++ examples/opensearch.tf
@@ -3,13 +3,13 @@ module "s3" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0" # use the latest release
 
   # Tags
-  business-unit          = var.business_unit
+  business_unit          = var.business_unit
   application            = var.application
-  is-production          = var.is_production
+  is_production          = var.is_production
   team_name              = var.team_name
   namespace              = var.namespace
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
 
 # Create the domain
