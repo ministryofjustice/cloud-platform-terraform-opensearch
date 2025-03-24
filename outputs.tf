ac37a84e6c2c87ba0dc82bce0c3adc187cd583c8
Marcus Aspin <maspin@unilink.com>
2025-03-24 11:07:29 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,7 +15,7 @@ output "irsa_role_arn" {
 
 output "domain_arn" {
   description = "OpenSearch domain ARN"
-  value       = aws_opensearch_domain.this.endpoint
+  value       = aws_opensearch_domain.this.arn
 }
 
 output "endpoint" {
